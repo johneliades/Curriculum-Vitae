b7dfb6d508deb8d06f18c4c32982f5944f8a66e3
johneliades <36938242+johneliades@users.noreply.github.com>
2019-12-12 03:35:17 +0200

    New menu font

--- js/events.js
+++ js/events.js
@@ -9,20 +9,17 @@ function color_change() {
 	var i;
 
 	var element_headers = document.querySelectorAll("h2");
-	if(element_headers)
-		for (i=0; i<element_headers.length; i++)
-			element_headers[i].classList.toggle("color");
+	for (i=0; i<element_headers.length; i++)
+		element_headers[i].classList.toggle("color");
 	
 	var element_text = document.querySelectorAll("p.custom_font");
-	if(element_text)
-		for (i=0; i<element_text.length; i++)
-			element_text[i].classList.toggle("custom_font_color");
+	for (i=0; i<element_text.length; i++)
+		element_text[i].classList.toggle("custom_font_color");
 	
 	var element_buttons = document.querySelectorAll("a.button");
-	if(element_buttons)
-		for (i=0; i<element_buttons.length; i++)
-			element_buttons[i].classList.toggle("button_color");
-
+	for(i=0; i<element_buttons.length; i++)
+		element_buttons[i].classList.toggle("button_color");
+	
 	var background_body = document.getElementsByTagName("body")[0];
 	if(background_body)
 		background_body.classList.toggle("color");
@@ -32,8 +29,6 @@ function color_change() {
 		background_html.classList.toggle("color");
 	
 	var border = document.getElementsByClassName("column");
-	if(border)
-		for (i=0; i<element_buttons.length; i++)
-			border[i].classList.toggle("border");
-
+	for(i=0; i<border.length; i++)
+		border[i].classList.toggle("border");
 }

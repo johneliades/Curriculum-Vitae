0d221de7aa89f58d46c082d4f54f650f553e4106
johneliades <johneliades@hotmail.com>
2021-11-30 18:42:54 +0200

    Revert "faster quotes attempt"
    This reverts commit e50e1f3949c436ddf0fdac2ae5b263666bc0723f.

--- js/index.js
+++ js/index.js
@@ -60,7 +60,7 @@ $("#bird_container").click(function() {
 	var randomItem = myArray[generateUniqueRandom(myArray.length-1)];
 
 	iframe=document.getElementById('iframe_main');
-	iframe.contentWindow.document.getElementById('quote').innerText = randomItem;
+	iframe.src = "quote_page.html?data="+randomItem;
 })
 
 <!-- Disables the bird -->
@@ -121,8 +121,7 @@ function initFrame() {
 		bird_container.style.zIndex = 0;
 
 	iframe=document.getElementById('iframe_main');
-	iframe.src = "quote_page.html";
-	iframe.contentWindow.document.getElementById('quote').innerText = "Shoot the bird & enjoy the quote ;)"
+	iframe.src = "quote_page.html?data=Shoot the bird & enjoy the quote ;)";
 
 	$('#main_screen').fadeIn("slow");
 	$("#iframe_main").focus();

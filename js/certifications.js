4d2c37fa1353f20825438617ff42474a3f265132
johneliades <johneliades@hotmail.com>
2022-12-15 20:57:19 +0200

    added certifications category

---
+++ js/certifications.js
@@ -0,0 +1,51 @@
+var certificateList = [
+	{ file: "documents/degree.pdf", title: 'Degree of Electrical and Computer Engineering' },
+	{ file: "documents/gep.pdf", title: '80th National Guard Higher Command' },
+];
+
+const certificateFiles = certificateList.map(certificate => certificate.file);
+const certificateTitles = certificateList.map(certificate => certificate.title);
+
+var currentCertificate = 0;
+
+var certificateCounter = document.querySelector('#certificate-counter');
+certificateCounter.textContent = "" + (currentCertificate + 1) + "/" + certificateFiles.length;
+
+var certificateTitle = document.querySelector('#certificate-title');
+certificateTitle.textContent = certificateTitles[0];
+
+var certificateIcon = document.querySelector('#certificate-icon');
+img.src = 'new-image.jpg';
+
+function previousCertificate() {
+	if (currentCertificate > 0) {
+		currentCertificate--;
+		updateCertificate();
+	}
+}
+
+function nextCertificate() {
+	if (currentCertificate < certificateFiles.length - 1) {
+		currentCertificate++;
+		updateCertificate();
+	}
+}
+
+function updateCertificate() {
+	var certificateFrame = document.querySelector('#certificate-frame');
+	certificateFrame.src = certificateFiles[currentCertificate];
+	
+	var certificateCounter = document.querySelector('#certificate-counter');
+	certificateCounter.textContent = "" + (currentCertificate + 1) + "/" + certificateFiles.length;
+
+	var certificateTitle = document.querySelector('#certificate-title');
+	certificateTitle.textContent = certificateTitles[currentCertificate];
+}
+
+document.addEventListener('keydown', function(event) {
+	if (event.keyCode === 37) { // Left arrow key
+		previousCertificate();
+	} else if (event.keyCode === 39) { // Right arrow key
+		nextCertificate();
+	}
+});

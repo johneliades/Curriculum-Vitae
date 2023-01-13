059729aedc065abf42908335dce91be66d664378
johneliades <johneliades@hotmail.com>
2023-01-13 21:49:53 +0200

    minor fixes

--- js/index.js
+++ js/index.js
@@ -1,7 +1,5 @@
 <!-- Shows the welcome message and then enables the bird -->
 $("#profile_pic").click(function() {
-	document.title = "[John@Portfolio]-[~]"
-
 	iframe = document.getElementById('iframe_main');
 	iframe.src = "quote_page.html";
 
@@ -10,20 +8,7 @@ $("#profile_pic").click(function() {
 	$("#iframe_main").focus();
 });
 
-$(".change_title").click(function() {
-	var href = $(this).attr('href');
-	href = href.replace('documents/','');
-	href = 	href.replace('.html','');
-	href = 	href.replace('.png','');
-	href = 	href.replace('.pdf','');
-
-	href = href.substr(0,1).toUpperCase() + href.substr(1);
-
-	document.title = "[John@Portfolio]-[" + href + "]";
-
-	if(href == "CV") {
-		$('#iframe_main').hide();
-		$('#iframe_main').fadeIn("slow");
-	}
+$("#iframe_main").on('load',function() {
+	document.title = document.getElementById("iframe_main").contentDocument.title;
 	$("#iframe_main").focus();
-})
+});

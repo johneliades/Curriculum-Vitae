c6d7244afbde6b57cc61332b6735bf00b791ac71
johneliades <johneliades@hotmail.com>
2024-03-23 05:47:55 +0200

    Fix img scroll for phone

--- js/portfolio.js
+++ js/portfolio.js
@@ -91,3 +91,28 @@ $(window).on("wheel", function () {
     $(window).off("wheel");
   }
 });
+
+var startY;
+
+$(window).on("touchstart", function (event) {
+  var touch = event.originalEvent.touches[0];
+  startY = touch.clientY;
+});
+
+$(window).on("touchmove", function (event) {
+  if (!startY) {
+    return;
+  }
+
+  var touch = event.originalEvent.touches[0];
+  var deltaY = touch.clientY - startY;
+
+  if (deltaY < 0) {
+    $("#img_scroll").fadeOut(800);
+    $(window).off("touchmove");
+  }
+});
+
+$(window).on("touchend", function () {
+  startY = null;
+});
